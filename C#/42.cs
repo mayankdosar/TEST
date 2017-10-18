3b16bb700feca098accfcfdb2286ea3fe2dbf047
lavina <lavinajain.lj@gmail.com>
2017-10-19 00:57:56 +0530

    Added C# solution

---
+++ C#/42.cs
@@ -0,0 +1,20 @@
+using System;
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int i = 1;
+            while(i>0)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n == 42)
+                {
+                    i = 0;
+                }
+            }
+        }
+    }
+}
